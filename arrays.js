c3b37b6d0c57f7041d7439f59ec3e97c8b8dc743
Christopher Lam <christopher.lam@students.mq.edu.au>
2017-11-29 23:21:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,3 +18,18 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  var arr2 = [...array, element]
+  return arr2
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index) {
+  var arr2 = [array]
+  return arr2[index]
+}
